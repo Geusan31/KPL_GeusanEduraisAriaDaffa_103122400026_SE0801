bde84a943b709a7ff188240be50f387271af62e6
Geusan Edurais <edurais.ariadaffa@gmail.com>
2026-03-03 10:15:02 +0700

    feat: update array index js

--- 02_Pemrograman_Javascript/index.js
+++ 02_Pemrograman_Javascript/index.js
@@ -1,14 +1,14 @@
-const arr1 = [1, -2,3,-4,5,-6]
+const arr1 = [2, 26, 28, -2];
 
 function mulOfArray(arr) {
-    let result = 1
-    for (let i = 0; i < arr.length; i++) {
-        if(arr[i] >= 0) {
-            result *= arr[i]
-        }
+  let result = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 0) {
+      result *= arr[i];
     }
-    return result
+  }
+  return result;
 }
 
-const arr1Result = mulOfArray(arr1)
-console.log(arr1Result)
+const arr1Result = mulOfArray(arr1);
+console.log(arr1Result);
